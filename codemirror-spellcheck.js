052be9fb8974f84ee463b4a371616626bcbf8944
Animation Prep Studios <jython.scripts@gmail.com>
2021-01-01 23:59:20 -0500

    Update codemirror-spellcheck.js

--- codemirror-spellcheck.js
+++ codemirror-spellcheck.js
@@ -2,4 +2,4 @@ function EnableSpellCheck() {
 	var x = document.getElementsByClassName("CodeMirror-code");
 	x[0].setAttribute("spellcheck", "on"); 
 }
-setTimeout(EnableSpellCheck, 3000); //very ugly, wait for XHR so the div with CodeMirror-code class will be created.
+setTimeout(EnableSpellCheck, 3000); //very ugly, wait for XHR so the div with CodeMirror-code class will have time to be created.
