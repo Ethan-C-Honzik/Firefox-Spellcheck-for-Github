a09daf2f941d5e425306c62f8d9f9c6cf7cdfe29
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2024-04-27 20:45:57 -0500

    updated logs

--- codemirror-spellcheck.js
+++ codemirror-spellcheck.js
@@ -1,10 +1,11 @@
-console.log("Spellcheck loaded")
+console.log("Spellcheck Extension Loaded");
 
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 async function EnableSpellCheck() {
 	let x = document.getElementsByClassName("cm-content");
 	while(x == null) await sleep(1000);
+	console.log("Spellcheck Loaded");
 	x[0].setAttribute("spellcheck", "on"); 
 }
 
