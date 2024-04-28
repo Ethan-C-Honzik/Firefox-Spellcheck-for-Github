ab681544ab5f44ee6f61b735dc7be29ca8348f39
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2024-04-27 20:49:43 -0500

    fix race condition

--- codemirror-spellcheck.js
+++ codemirror-spellcheck.js
@@ -4,7 +4,7 @@ const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 async function EnableSpellCheck() {
 	let x = document.getElementsByClassName("cm-content");
-	while(x == null) await sleep(1000);
+	while(x == null || x[0] == null) await sleep(1000);
 	console.log("Spellcheck Loaded");
 	x[0].setAttribute("spellcheck", "on"); 
 }
